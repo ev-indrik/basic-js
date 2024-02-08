8362f8f1fba17e8b10dad0db780457e0e6c0b340
ev-indrik <delfinmur@gmail.com>
2024-02-08 22:15:23 +0200

    DOM changes

---
+++ 01-test-tasks/js/dom.js
@@ -0,0 +1,9 @@
+// document navigation
+
+const htmlElement = document.documentElement;
+// console.log(htmlElement);
+
+const headElement = document.head;
+const bodyElement = document.body;
+// console.log(headElement);
+// console.log(bodyElement);
